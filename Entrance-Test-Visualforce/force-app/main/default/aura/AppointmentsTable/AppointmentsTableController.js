df120a6e31e1f37ef5427fd99efc76ab28fb2768
Denis Slavenko <denis-slavenko1999@mail.ru>
2021-06-17 15:21:38 +0300

    Replaced <lightning:select> with <lightning:combobox>

--- Entrance-Test-Visualforce/force-app/main/default/aura/AppointmentsTable/AppointmentsTableController.js
+++ Entrance-Test-Visualforce/force-app/main/default/aura/AppointmentsTable/AppointmentsTableController.js
@@ -1,13 +1,13 @@
 ({
     init : function (cmp, event, helper) {
         cmp.set('v.columns', [
-            { label: 'Action', fieldName: 'viewLink', type: 'url', typeAttributes: { 
+            { label: 'Action', fieldName: 'viewLink', type: 'url', typeAttributes: {
                 label: 'View',
-                tooltip: { fieldName: 'tooltip' }, 
-                target: '_self' } 
+                tooltip: { fieldName: 'tooltip' },
+                target: '_self' }
             },
             { label: 'Doctor\'s Name', fieldName: 'DoctorName', type: 'text' },
-            { label: 'Patient\'s Name', fieldName: 'PatientName', type: 'text' },            
+            { label: 'Patient\'s Name', fieldName: 'PatientName', type: 'text' },
             { label: 'Date', fieldName: 'Appointment_Date__c', type: 'date', typeAttributes: {
                 day: 'numeric',
                 month: 'short',
@@ -19,96 +19,96 @@
                 timeZone: 'UTC' }
             }, // FIX DATETIME DIFFERENCES
             { label: 'Duration', fieldName: 'Duration_in_minutes__c', type: 'number' },
-            { type: 'button', typeAttributes: { 
-                iconName: 'utility:delete', 
-                label: 'Delete', 
-                name: 'Delete', 
+            { type: 'button', typeAttributes: {
+                iconName: 'utility:delete',
+                label: 'Delete',
+                name: 'Delete',
                 variant: 'destructive' }
             }
         ]);
-        
+
         helper.getDoctors(cmp);
         helper.getPatients(cmp);
         helper.getData(cmp);
         helper.getToday(cmp);
     },
-    
+
     /*getSelectedRow : function (cmp, event, helper) {
         var selectedRow = event.getParam("selectedRow");
         cmp.set("v.selectedRow", selectedRow);
     },*/
-    
+
     handleRowAction : function (cmp, event, helper) {
         helper.deleteRecordAction(cmp, event, helper);
     },
-    
+
     handleHideDialog : function (cmp, event, helper) {
         helper.hideConfirmDialog(cmp, event, helper);
     },
-    
+
     /*handleConfirmDialogYes : function (cmp, event, helper) {
         console.log('--- EVENT 1 ---');
-        
+
         helper.hideConfirmDialog(cmp, event, helper);
     },*/
-    
-    onDoctorChange : function (cmp, event, helper) {       
+
+    onDoctorChange : function (cmp, event, helper) {
         helper.doctorChanged(cmp, event);
     },
-    
-    onPatientChange : function (cmp, event, helper) {       
+
+    onPatientChange : function (cmp, event, helper) {
         helper.patientChanged(cmp, event);
     },
-    
+
     onAppointmentDateChange : function (cmp, event, helper) {
         helper.appointmentDateChanged(cmp, event);
     },
-    
+
     onDurationChange : function (cmp, event, helper) {
         helper.durationChanged(cmp, event);
     },
-    
+
     onPagesizeChange : function (cmp, event, helper) {
         helper.pagesizeChanged(cmp, event);
     },
-    
+
     clearDoctorSelect : function (cmp, event, helper) {
         helper.doctorSelectCleared(cmp, event);
     },
-    
+
     clearPatientSelect : function (cmp, event, helper) {
         helper.patientSelectCleared (cmp, event);
     },
-    
+
     clearDuration : function (cmp, event, helper) {
         helper.durationCleared (cmp, event);
     },
-    
-    handleNext : function (cmp, event, helper) { 
+
+    handleNext : function (cmp, event, helper) {
         var pageNumber = cmp.get("v.pageNumber");
         cmp.set("v.pageNumber", pageNumber + 1);
         helper.getData(cmp);
     },
-     
-    handlePrev : function (cmp, event, helper) {        
+
+    handlePrev : function (cmp, event, helper) {
         var pageNumber = cmp.get("v.pageNumber");
         cmp.set("v.pageNumber", pageNumber - 1);
         helper.getData(cmp);
     },
-    
+
     showWaiting : function(cmp){
         //console.log('Show Waiting');
     },
-    
+
     hideWaiting : function(cmp){
         //console.log('Hide Waiting');
     },
-    
+
     handleFocus : function(cmp){
-        
+
     },
-    
+
     handleBlur : function(cmp){
-        
+
     }
 });
--- Entrance-Test-Visualforce/force-app/main/default/aura/AppointmentsTable/AppointmentsTableHelper.js
+++ Entrance-Test-Visualforce/force-app/main/default/aura/AppointmentsTable/AppointmentsTableHelper.js
@@ -1,68 +1,68 @@
 ({
-    
+
     // реализовать апдейт даты
     // если доктор или пациент не выбран - вызывать стандартную
     // если выбраны - прописать кастомную логику
-    
+
     getData : function(cmp, doctorId, patientId, appointmentDate, duration, pageNumber, pageSize) {
         var action = cmp.get('c.getAppointments');
-        
+
         console.log('Page Number: ' + cmp.get("v.pageNumber"));
-        
+
         if (doctorId && typeof doctorId !== "undefined") {
             action.setParam("selectedDoctor", doctorId);
         }
-        
+
         if (patientId && typeof patientId !== "undefined") {
             action.setParam("selectedPatient", patientId);
         }
-        
+
         if (appointmentDate && typeof appointmentDate !== "undefined") {
             // решить проблему с преобразованием в datetime
             // нужно как-то получать не строку чистую, а сразу красивую дату
-            
+
             action.setParam("selectedDate", appointmentDate);
         }
-        
+
         if (duration && typeof duration !== "undefined") {
             action.setParam("selectedDuration", duration);
         }
-        
+
         var pageNumber = cmp.get("v.pageNumber");
         if(pageNumber) {
             action.setParam("pageNumber", pageNumber);
         }
-        
+
         var pageSize = cmp.find("pageSize").get("v.value");
         if(pageSize) {
             action.setParam("pageSize", pageSize);
         }
-        
+
         action.setCallback(this, $A.getCallback(function (response) {
             var state = response.getState();
-            
+
             if (state === 'SUCCESS') {
                 var resultData = response.getReturnValue();
-                
+
                 for (var i = 0; i < resultData.length; i++) {
                     var row = resultData[i];
-                    
+
                     row.viewLink = '/' + row.Id;
                     row.tooltip = row.Name;
-                    
+
                     if (row.Doctor__c) {
                         row.DoctorName = row.Doctor__r.Name;
                     }
-                    
+
                     if (row.Patient__c) {
                         row.PatientName = row.Patient__r.Name;
                     }
-                    
+
                     /*if (row.Appointment_Date__c) {
                         row.FormattedDate = $A.localizationService.formatDate(row.Appointment_Date__c, "MMM dd yyyy, hh:mm:ss a");
                     }*/
                 }
-                
+
                 cmp.set('v.data', resultData);
                 //cmp.set("v.totalPages", Math.ceil(resultData.length / pageSize));
             } else if (state === 'ERROR') {
@@ -70,109 +70,128 @@
                 console.error(errors);
             }
         }));
-        
+
         $A.enqueueAction(action);
     },
-    
+
     getDoctors : function(cmp) {
         var action = cmp.get('c.getDoctors');
-        
+
         action.setCallback(this, $A.getCallback(function (response) {
             var state = response.getState();
-            
+
             if (state === 'SUCCESS') {
                 var doctors = response.getReturnValue();
-                
-                cmp.set('v.doctors', doctors);
+
+                var options = [{ "label": "All", "value": "" }];
+
+                for (var i = 0; i < doctors.length; i++) {
+                    var option = {
+                        "label": doctors[i].Name,
+                        "value": doctors[i].Id
+                    };
+                    options.push(option);
+                };
+
+                cmp.set('v.doctors', options);
             }
         }));
         $A.enqueueAction(action);
     },
-    
+
     getPatients : function(cmp) {
         var action = cmp.get('c.getPatients');
-        
+
         action.setCallback(this, $A.getCallback(function (response) {
             var state = response.getState();
-            
+
             if (state === 'SUCCESS') {
                 var patients = response.getReturnValue();
-                
-                cmp.set('v.patients', patients);
+
+                var options = [{ "label": "All", "value": "" }];
+
+                for (var i = 0; i < patients.length; i++) {
+                    var option = {
+                        "label": patients[i].Name,
+                        "value": patients[i].Id
+                    };
+                    options.push(option);
+                };
+
+
+                cmp.set('v.patients', options);
             }
         }));
-        
+
         $A.enqueueAction(action);
     },
-    
+
     getToday : function(cmp) {
         var today = $A.localizationService.formatDateTime(new Date(), "YYYY-MM-DD\'T\'HH:mm:ss.SSSZ");
-        
+
         cmp.set('v.datetime', today);
-        
+
         // 1. ПОЛУЧАТЬ СРАЗУ UTC-ФОРМАТ
         // 2. КОНВЕРТИРОВАТЬ СРАЗУ В НОРМАЛЬНЫЙ ISO (ЗАГУГЛИТЬ)
     },
-    
+
     updateData : function(cmp) {
         var doctorId = cmp.find('doctorId').get('v.value');
         var patientId = cmp.find('patientId').get('v.value');
         var appointmentDate = cmp.find('appointmentDate').get('v.value');
         var duration = cmp.find('duration').get('v.value');
-        var test = cmp.find('doctorIdTest').get('v.value');
-        console.log('Combobox value: ' + test);
-        
+
         /*var dt = new Date();
         var ndt = $A.localizationService.formatDateTime(new Date(dt.getFullYear(), dt.getMonth(), dt.getDate()), "YYYY-MM-DD\'T\'HH:mm:ss.SSSZ");
         var ndt2 = $A.localizationService.formatDateTime(new Date(dt.getFullYear(), dt.getMonth(), dt.getDate()+1), "YYYY-MM-DD\'T\'HH:mm:ss.SSSZ");
         console.log(dt);
         console.log(ndt);
         console.log(ndt2);*/
-        
+
         this.getData(cmp, doctorId, patientId, appointmentDate, duration);
     },
-    
+
     doctorChanged : function (cmp, event) {
         //var doctorId = cmp.find('doctorId').get('v.value');
-        
+
         this.updateData(cmp);
     },
-    
+
     patientChanged : function (cmp, event) {
         //var patientId = cmp.find('patientId').get('v.value');
-        
+
         this.updateData(cmp);
     },
-    
+
     appointmentDateChanged : function (cmp, event) {
         this.updateData(cmp);
     },
-    
+
     durationChanged : function (cmp, event) {
         this.updateData(cmp);
     },
-    
+
     deleteRecordAction : function (cmp, event) {
         //var recordId = event.getParam("row").Id;
         var actionName = event.getParam("action").name;
         var record = event.getParams().row;
-        
+
         if (actionName === "Delete") {
             this.showConfirmDialog(cmp);
         }
     },
-    
+
     deleteRecord : function (cmp, event) {
         var confirmed = cmp.get("v.allowedDeletion");
         console.log(confirmed);
-        
+
         if(confirmed) {
             var action = cmp.get('c.deleteAppointment');
-            
+
             action.setParams({
                 //appointmentToDelete: JSON.stringify(record)
             });
-            
+
             action.setCallback(this, function(response) {
                 var state = response.getState();
                 if (state === "SUCCESS") {
@@ -185,17 +204,17 @@
             })
             $A.enqueueAction(action);
         }
-        
+
         cmp.set("v.allowedDelition", false);
     },
-        
-    /*var paramStr = JSON.stringify(event.getParams(), null, 4); 
+
+    /*var paramStr = JSON.stringify(event.getParams(), null, 4);
     console.log(paramStr);*/
-    
+
     showConfirmDialog : function (cmp, event, helper) {
         cmp.set('v.showConfirmDialog', true);
     },
-    
+
     hideConfirmDialog : function (cmp, event, helper) {
         cmp.set("v.showConfirmDialog", false);
     },
@@ -204,26 +223,26 @@
         cmp.set("v.allowedDelition", true);
         cmp.set('v.showConfirmDialog', false);
     },
-    
+
     doctorSelectCleared : function (cmp, event) {
         cmp.find('doctorId').set('v.value', '');
         this.updateData(cmp);
     },
-    
+
     patientSelectCleared : function (cmp, event) {
         cmp.find('patientId').set('v.value', '');
         this.updateData(cmp);
     },
-    
+
     durationCleared : function (cmp, event) {
         cmp.find('duration').set('v.value', '');
         this.updateData(cmp);
     },
-    
+
     pagesizeChanged : function (cmp, event) {
         this.updateData(cmp);
     },
-    
+
     // Toast Event накладывается сверху на другой Toast Event только тогда,
     // когда в их сообщении находится разный текст
     showToastSuccessfulDelete : function(cmp, event, record) {
