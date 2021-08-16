a8b4194830ca519860a934d161a34d53b18e34f7
Denis Slavenko <denis-slavenko1999@mail.ru>
2021-08-16 18:27:18 +0300

    Added additional functionality

--- Entrance-Test-Visualforce/force-app/main/default/lwc/appointmentsTableLWC/appointmentsTableLWC.js
+++ Entrance-Test-Visualforce/force-app/main/default/lwc/appointmentsTableLWC/appointmentsTableLWC.js
@@ -2,7 +2,7 @@ import { LightningElement, api, track, wire } from 'lwc';
 import utils from 'c/utils';
 import timeZone from '@salesforce/i18n/timeZone';
 import getDoctors from '@salesforce/apex/AppointmentsControllerLWC.getDoctors';
-import getWorkingHours from '@salesforce/apex/AppointmentsControllerLWC.getDoctors';
+import getWorkingHours from '@salesforce/apex/AppointmentsControllerLWC.getWorkingHours';
 import getPatients from '@salesforce/apex/AppointmentsControllerLWC.getPatients';
 import getAppointments from '@salesforce/apex/AppointmentsControllerLWC.getAppointments';
 
@@ -47,10 +47,25 @@ export default class AppointmentsTableLWC extends LightningElement {
     pageSizes = PAGE_SIZES;
     @track selectedPagesize = this.pageSizes[0].value;
 
-    @track doctors;
-    @track patients;
-    @track selectedDoctor;
-    @track selectedPatient;
+    @track doctors = '';
+    @track patients = '';
+    @track selectedDoctor = '';
+    @track selectedPatient = '';
+    @track datetime = '';
+    @track duration = '';
+
+    @track pageNumber = 1;
+    @track totalRecords = '';
+    @track recordStart = '';
+    @track recordEnd = '';
+    @track totalPages = '';
+    @track totalPages = '';
+
+    @track disabledNextButton;
+    @track disabledPrevButton;
+
+    @track startingRecordNumber;
+    @track endingRecordNumber;
 
     @track appointments;
 
@@ -110,7 +125,7 @@ export default class AppointmentsTableLWC extends LightningElement {
         }
     }
 
-    @wire(getAppointments, {selectedDoctor: '$selectedDoctor', selectedPatient: '$selectedPatient', pageSize: '$selectedPagesize'})
+    @wire(getAppointments, {selectedDoctor: '$selectedDoctor', selectedPatient: '$selectedPatient', selectedDatetime: '$datetime', selectedDuration: '$duration', pageNumber: '$pageNumber', pageSize: '$selectedPagesize'})
     wiredAppointments({ error, data }) {
         if (data) {
             let tempRecordsList = [];
@@ -119,6 +134,26 @@ export default class AppointmentsTableLWC extends LightningElement {
                 this.displayData = true;
             } else {
                 this.displayData = false;
+            }
+
+            this.totalRecords = data.totalRecords;
+            this.recordStart = data.recordStart;
+            this.recordEnd = data.recordEnd;
+            this.totalPages = Math.ceil(data.totalRecords / this.selectedPagesize);
+
+            this.startingRecordNumber = (this.pageNumber - 1) * this.selectedPagesize;
+            this.endingRecordNumber = (this.pageNumber - 1) * this.selectedPagesize + data.appointments.length;
+
+            if (this.pageNumber == this.totalPages) {
+                this.disabledNextButton = true;
+            } else {
+                this.disabledNextButton = false;
+            }
+
+            if (this.pageNumber == 1) {
+                this.disabledPrevButton = true;
+            } else {
+                this.disabledPrevButton = false;
             }
 
             data.appointments.forEach(row => {
@@ -139,24 +174,83 @@ export default class AppointmentsTableLWC extends LightningElement {
         }
     }
 
+    onSetToday() {
+        let today = new Date();
+
+        this.datetime = today.toISOString();
+
+        // this.pageNumber = 1;
+    }
+
     onDoctorChange(event) {
         this.selectedDoctor = event.target.value;
+
+        this.pageNumber = 1;
     }
 
     onPatientChange(event) {
         this.selectedPatient = event.target.value;
+
+        this.pageNumber = 1;
+    }
+
+    onAppointmentDatetimeChange(event) {
+        this.datetime = event.target.value;
+
+        this.pageNumber = 1;
+    }
+
+    onDurationChange(event) {
+        this.duration = event.target.value;
+
+        this.pageNumber = 1;
     }
 
     onPagesizeChange(event) {
         this.selectedPagesize = event.target.value;
+
+        this.pageNumber = 1;
     }
 
     onDoctorClear() {
         this.selectedDoctor = '';
+
+        this.pageNumber = 1;
     }
 
     onPatientClear() {
         this.selectedPatient = '';
+
+        this.pageNumber = 1;
+    }
+
+    onAppointmentDatetimeClear() {
+        this.datetime = '';
+
+        this.pageNumber = 1;
+    }
+
+    onDurationClear() {
+        this.duration = null;
+
+        this.pageNumber = 1;
+    }
+
+    handleRowAction(event) {
+        let actionName = event.detail.action.name;
+        let record = event.detail.row;
+
+        if (actionName === 'Delete') {
+            console.log('Delete action');
+        }
+    }
+
+    handleClickNext() {
+        this.pageNumber = this.pageNumber + 1;
+    }
+
+    handleClickPrev() {
+        this.pageNumber = this.pageNumber - 1;
     }
 
 }
